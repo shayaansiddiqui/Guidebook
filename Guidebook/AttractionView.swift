2a29a547b708d9b1e30729e86d85270f8486182f
Shayaan Siddiqui <shayaan.siddiqui@gmail.com>
2025-10-27 10:39:56 -0400

    Initial Project Setup

---
+++ Guidebook/AttractionView.swift
@@ -0,0 +1,18 @@
+//
+//  AttractionView.swift
+//  Guidebook
+//
+//  Created by Shayaan Siddiqui on 10/27/25.
+//
+
+import SwiftUI
+
+struct AttractionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AttractionView()
+}
---
+++ Guidebook/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Guidebook
+//
+//  Created by Shayaan Siddiqui on 10/27/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
