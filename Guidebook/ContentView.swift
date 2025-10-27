dfea59096a6e10f1bef3a262f36582c5aa9e3938
Shayaan Siddiqui <shayaan.siddiqui@gmail.com>
2025-10-27 10:37:02 -0400

    Initial Commit

---
+++ Guidebook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guidebook
+//
+//  Created by Shayaan Siddiqui on 10/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Shayaan Siddiqui on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
